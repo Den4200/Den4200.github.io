5b172e129556f3f9e32aa4114995d6a439e66dae
Den4200 <dpham.42@hotmail.com>
2019-12-08 22:02:36 -0500

    under development

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Scripts
+
+var typed = new Typed('#typing', {
+    strings: [
+        "<strong>a developer</strong>.", 
+        "<strong>a guitarist</strong>.", 
+        "<strong>a student</strong>."
+    ],
+    typeSpeed: 40,
+    backSpeed: 30,
+    showCursor: false,
+    loop: true
+});
